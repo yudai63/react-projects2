a44bf43e3bd719873b70928bde9813576774ac7c
Yuudai Tamura <y.tamu63@gmail.com>
2024-12-26 17:31:01 +0900

    react router,Chakra UIの導入

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import { Button, VStack } from "@chakra-ui/react";
+import Home from "./pages/Home";
+import AboutContact from "./pages/AboutContact";
+import Films from "./pages/Films";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export default function App(){
+    return (
+        <>
+            <Header />
+            <VStack spacing={4} align="stretch" minH="100vh">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about-contact" element={<AboutContact />} />
+                    <Route path="/films" element={<Films />} />
+                </Routes>
+            </VStack>
+            <Footer />
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,12 @@
 import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-createRoot(document.querySelector("#content")).render(<App />);
+createRoot(document.querySelector("#content")).render(
+  <ChakraProvider>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </ChakraProvider>
+);
